90f39141f7549196de6d19570ea8b9392ee09f16
Long Vien <vienngoclongvn@gmail.com>
2021-09-30 07:38:15 +0700

    completed exercise

---
+++ Practice/whilenameandage.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+age = input("How old are you?")
+counter = 0
+while counter < 4:
+    print("My name is " + name + " , I'm " + age + " years old. Nice to meet you.")
+    counter = counter + 1
