e5a5ac93a5acc05c9c59dcee99a8ab2a7d708e4d
Long Vien <vienngoclongvn@gmail.com>
2021-10-08 13:55:59 +0700

    completed exercise

--- Practice/HappybirthdaymeHang.py
+++ Practice/HappybirthdaymeHang.py
@@ -1,5 +1,6 @@
 name = input(" Tên bạn là gì?")
 age = input(" Năm nay bạn bao nhiêu tuổi?")
-print(" Chúc mừng sinh nhật " + name + " , Năm nay bạn " + age + " tuổi. Chúc bạn tuổi mới mạnh khoẻ và vui vẻ.")
 if int(age) > 100:
     print(" Hệ thống lỗi! Tuổi không hợp lệ!")
+else:
+    print(" Chúc mừng sinh nhật " + name + " , Năm nay bạn " + age + " tuổi. Chúc bạn tuổi mới mạnh khoẻ và vui vẻ.")
