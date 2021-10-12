ea486763b5cf37c78ffecdb21f6964af85969000
Long Vien <vienngoclongvn@gmail.com>
2021-10-12 07:45:50 +0700

    completed exercise

---
+++ Practice/vaccinenumber.py
@@ -0,0 +1,6 @@
+vaccine1 = input("What's is the score for vaccine1?")
+vaccine2 = input("What's is the score for vaccine2?")
+if vaccine1 > vaccine2:
+    print("vaccine 1 is stronger than vaccine 2.")
+else:
+    print("vaccine 1 is weaker than vaccine 2.")
