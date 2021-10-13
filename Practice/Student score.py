05cbd2bfb8e15c777a4a93dec39de22ef575b277
Long Vien <vienngoclongvn@gmail.com>
2021-10-13 08:05:29 +0700

    commpleted exercise

---
+++ Practice/Student score.py
@@ -0,0 +1,6 @@
+student1 = input("What's is the score for student 1 ?")
+student2 = input("What's is the score for student 2?")
+if student1 > student2:
+    print("student 1 score is higher than student 2.")
+else:
+    print("student 1 is score is less than student2.")
