dfa7c6db2513ea0f1150addd12d4377ba883bb37
Long Vien <vienngoclongvn@gmail.com>
2021-10-25 07:27:59 +0700

    completed exercise

---
+++ Practice/Halloweenvideo.py
@@ -0,0 +1,3 @@
+AI = input("Did you know Halloween came from Ireland? Type: No or no do not type  yes because  this game is for who don not know  Halloween")
+if AI == " no" or "No":
+        print("Please watch this video and learn more about Halloween!!!!!!!!!!!!! Link: https://www.youtube.com/watch?v=F-TR35esdMA")
