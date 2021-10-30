8436361c60ceb522404c51d9bbf943c5e5aacd5a
Long Vien <vienngoclongvn@gmail.com>
2021-10-30 15:47:44 +0700

    completed exercise

--- Practice/printenvennumbers.py
+++ Practice/printenvennumbers.py
@@ -1,6 +1,6 @@
 array = input("Enter an array: Ex: 1 2 3 4 5. ")
-numbers = array.split("\\s+")
-print(numbers)
+numbers = array.split( )
+
 for number in numbers:
     if int(number)%2 == 0:
         print(number + " ")
