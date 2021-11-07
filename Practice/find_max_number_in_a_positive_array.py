19bbb3f90c9c3323b9ab96615f4496f916cddea5
Long Vien <vienngoclongvn@gmail.com>
2021-11-07 12:08:46 +0700

    completed exercise

---
+++ Practice/find_max_number_in_a_positive_array.py
@@ -0,0 +1,6 @@
+array = input("Enter a positive array (Ex: 1 2 3 4): ")
+numbers = array.split()
+max_number = -1
+for number in numbers:
+    max_number = max(max_number,int(number))
+print("max_number is: " + str(max_number))
