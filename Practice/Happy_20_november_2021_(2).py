5b766af66167231520776e0f13dbdbe932b753c0
Long Vien <vienngoclongvn@gmail.com>
2021-11-12 11:00:24 +0700

    completed exercise

--- Practice/Happy_20_november_2021_(2).py
+++ Practice/Happy_20_november_2021_(2).py
@@ -1,6 +1,7 @@
 import datetime as dt
 
 date = input("What's the date today? Ex: 2021-11-20 ")
+job = input("What's your job? Ex: Teacher")
 date_obj = dt.datetime.strptime(date,'%Y-%m-%d')
 date_obj = date_obj.strftime('%Y-%m-%d')
 print("Input date: " + date_obj)
@@ -10,9 +11,9 @@ print("Today: " + date_today)
 
 if date_obj == date_today:
     print("Input date is today date.")
-    if date_today == '2021-11-20':
+    if job == "Teacher" and date_today == "2021-11-20"
         print("Happy 20/11!")
         
 else:
-    print("Sorry, input date is not today date.")
+    print("Sorry, input date is not today date and you are not the teacher.")
 
