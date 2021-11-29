7b941c2b2814ca8049b05d4ad9222e11e5a91f5b
Long Vien <vienngoclongvn@gmail.com>
2021-11-29 12:00:10 +0700

    completed exercise

---
+++ Practice/favourite_alien_charactor.py
@@ -0,0 +1,3 @@
+name = input("What's your name ?")
+cartoon = input("What's your favourite alien  charactor in Ben10 ?")
+print(" Hello " + name + " your favourite alien charactor is " + cartoon + ".")
