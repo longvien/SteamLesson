bed8d552c3e9ad710f8eaf969f8965c06617f758
Long Vien <vienngoclongvn@gmail.com>
2021-12-08 11:05:47 +0700

    completed exercise

---
+++ Practice/count_odd_numbers.py
@@ -0,0 +1,7 @@
+array = input("Enter an array: Ex: 1 2 3 4 5. ")
+numbers = array.split( )
+count = 0
+for number in numbers:
+    if int(number)%2 == 1:
+        count += 1
+print("The total of the even numbers is:" + str(count))
