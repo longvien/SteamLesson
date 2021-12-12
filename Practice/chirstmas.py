00852044670b1d6ba15aef3f8cbd6e5ed0b206fe
Long Vien <vienngoclongvn@gmail.com>
2021-12-12 09:11:49 +0700

    completed exercise

--- Practice/chirstmas.py
+++ Practice/chirstmas.py
@@ -83,7 +83,7 @@ create_circle(oogway, 220, 180, 60, BG_COLOR)
 
 
 oogway.speed(10)
-number_of_stars = randint(90,100)
+number_of_stars = randint(990,1000)
 
 for _ in range(0,number_of_stars):
     x_star = randint(-(screen.window_width()//2),screen.window_width()//2)
