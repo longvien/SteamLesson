e710e208bc1f725c026aa8e9504803374efe7f78
Long Vien <vienngoclongvn@gmail.com>
2021-12-26 17:34:16 +0700

    completed exercise

---
+++ Practice/count_down_happy_new_year_2022.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+#Initialise settings
+start=5
+message=">      Happy New Year 2022!      <"
+
+#Start the countdown
+for counter in range(start,0,-1):
+    print(counter)
+    time.sleep(1)
+    os.system('clear')  
+
+#Display the message  
+print("v^v^v^v^v^v^v^v^v^v^v^v^v^v^v")
+print("<                           >")
+print(message)
+print("<                           >")
+print("v^v^v^v^v^v^v^v^v^v^v^v^v^v^v")
