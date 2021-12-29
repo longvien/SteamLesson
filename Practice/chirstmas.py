79681bf314c10fb637d48f667be7c84dd136606e
Long Vien <vienngoclongvn@gmail.com>
2021-12-29 11:09:39 +0700

    completed exercise

--- Practice/chirstmas.py
+++ Practice/chirstmas.py
@@ -81,7 +81,7 @@ create_circle(oogway, 230, 180, 60, "white")
 #create_circle(oogway, 220, 180, 60, BG_COLOR)
 
 oogway.speed(10)
-number_of_stars = randint(990,1000)
+number_of_stars = randint(9,10)
 
 for _ in range(0,number_of_stars):
     x_star = randint(-(screen.window_width()//2),screen.window_width()//2)
@@ -100,7 +100,7 @@ for _ in range(0,number_of_stars):
 
 oogway.speed(1)
 oogway.penup()
-msg = "Merry Christmas"
+msg = "Merry Christmas and Happy New Year!"
 oogway.goto(0, -200)  
 oogway.color("white")
 oogway.pendown()
