03c9dd8aa6b2c82f22717790234d4540a8c89740
Long Vien <vienngoclongvn@gmail.com>
2022-01-20 20:04:14 +0700

    completed exercise

---
+++ Practice/draw_picture.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+drawing_area = Screen()
+drawing_area.setup(width=750, height=500)
+
+shape('square')
+for i in range(75):
+    right(20 + i)
+    forward(1 + (i * 5))
+    right(40 + i)
+
+done()
