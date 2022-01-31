2d19e78d376b758bf1b0de0821c7f350999adde5
Long Vien <vienngoclongvn@gmail.com>
2022-01-31 09:39:51 +0700

    completed exercise

--- Practice/draw_picture.py
+++ Practice/draw_picture.py
@@ -4,7 +4,7 @@ drawing_area = Screen()
 drawing_area.setup(width=750, height=500)
 
 shape('square')
-for i in range(76):
+for i in range(77):
     right(21 + i)
     forward(7 + (i * 6))
     right(41 + i)
