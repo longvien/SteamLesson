f56ea1b3bbd198fe2e2c1a6fba7d4b4df83387a1
Long Vien <vienngoclongvn@gmail.com>
2022-02-26 21:08:43 +0700

    completed exercise

--- Practice/draw_picture.py
+++ Practice/draw_picture.py
@@ -6,7 +6,7 @@ drawing_area.setup(width=750, height=500)
 shape('square')
 for i in range(88):
     right(22 + i)
-    forward(8 + (i * 27 ))
+    forward(8 + (i * 28 ))
     right(44 + i)
 
 done()
