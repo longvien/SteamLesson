4b47f4b519c5b8c58ae56e3a65af83f37f8cf5f3
Long Vien <vienngoclongvn@gmail.com>
2022-05-01 22:40:58 +0700

    completed exercise

--- Practice/draw_doraemon.py
+++ Practice/draw_doraemon.py
@@ -16,7 +16,7 @@ def eyes():
     tracer(False)
     a = 2.5
     for i in range(121):
-        if 0 <= i < 30 or 60 <= i < 96:
+        if 0 <= i < 30 or 60 <= i < 97:
             a -= 0.04
             lt(3)
             fd(a)
