66cc6eb1347262bdfb3f8a2c9d0c0abc0026e8b8
Long Vien <vienngoclongvn@gmail.com>
2022-05-11 08:47:05 +0700

    completed exercise

--- Practice/draw_pikachu.py
+++ Practice/draw_pikachu.py
@@ -13,7 +13,7 @@ class Pikachu:
         self.t = turtle.Turtle()
         t = self.t
         t.pensize(8)
-        t.speed(1240000000000000000)
+        t.speed(124000000000000000)
         t.ondrag(getPosition)
 
     def noTrace_goto(self, x, y):
